bb0cded63a2bfc59035d9bda4f9f1817614e822f
Andy Smith <7727226+andysmith26@users.noreply.github.com>
2025-11-30 17:10:14 -0500

    Import global styles in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import '../app.css';
+
   import { browser } from '$app/environment';
   import { page } from '$app/stores';
   import { setAppEnvContext } from '$lib/contexts/appEnv';
